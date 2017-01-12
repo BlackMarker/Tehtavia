d3adffeff9129d52fbad866377a9927c400b9841
Jube <Jube@DESKTOP-MKAEJ4T>
2017-01-12 15:23:59 +0200

    Teht3 valmis

--- Teht3/Teht3/Program.cs
+++ Teht3/Teht3/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Tee ohjelma, joka kysyy käyttäjältä kolme lukua ja tulostaa niiden summan ja keskiarvon.
+using System;
+
 
 namespace Teht3
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
+    class Program
+    {
+        static void Main(int[] args)
+        {
+            Console.WriteLine("Anna 3 lukue (Esim: '5 7 3'");
+            string rivi = Console.ReadLine();
+            string[] muuttujat = rivi.Split(' ');
+            int m1 = int.Parse(muuttujat[0]);
+            int m2 = int.Parse(muuttujat[0]);
+            int m3 = int.Parse(muuttujat[0]);
+
+            int tulos = (m1 + m2 + m3) / 2;
+            Console.Write(tulos);
+        }
+    }
 }
---
+++ Teht3Toimiva/Teht3Toimiva/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht3Toimiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna 3 lukua eroteltuma spacella (Esim: '5 7 3')");
+            string rivi = Console.ReadLine();
+            string[] muuttujat = rivi.Split(' ');
+            int m1 = int.Parse(muuttujat[0]);
+            int m2 = int.Parse(muuttujat[1]);
+            int m3 = int.Parse(muuttujat[2]);
+
+            int tulos1 = m1 + m2 + m3;
+            float apu = (float)tulos1;
+            float tulos2 = apu / 3;
+            Console.Write("Summa: " + tulos1 + " KA: " + tulos2 + "\n");
+        }
+    }
+}
