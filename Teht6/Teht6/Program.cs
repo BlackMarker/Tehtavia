456db759da759fafe7ca061a3f2f6239c654d461
Jube <Jube@DESKTOP-MKAEJ4T>
2017-01-12 18:43:03 +0200

    Teht7 valm

---
+++ Teht6/Teht6/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Auton kulutus on 7.02 litraa 100 kilometrin matkalla ja bensan hinta on 1.595 Euroa. 
+ * Tee ohjelma, joka tulostaa ajetulla matkalla (kysytään käyttäjältä) kuluvan bensan määrän sekä bensaan menevän rahan määrän.
+ */
+
+using System;
+
+namespace Teht6 {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Montako kilometria olet ajanut: ");
+			float matka;
+			matka = (float)int.Parse(Console.ReadLine());
+
+			float litraa = matka * 7.02f;
+			float hinta = litraa * 1.595f;
+			Console.WriteLine("Olet kuluttanut: \n" + litraa + " -Litraa\n" + hinta + " -euroa\n");
+		}
+	}
+}
---
+++ Teht7/Teht7/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Tee ohjelma, joka näyttää onko annettu vuosi karkausvuosi. Vuosiluku kysytään käyttäjältä.
+Algoritmi:
+
+4:llä jaolliset on, paitsi täydet vuosisadat. Kuitenkin 400:lla jaolliset vuosisadat ovat
+Esim. 1991 -> ei, 1992 -> on, 1900 -> ei, 2000 -> on
+*/
+using System;
+
+namespace Teht7 {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Anna vuosiluku, kerron onko se karkausvuosi: ");
+			int vuosi = int.Parse(Console.ReadLine());
+			//jos vuosi on 400 jaollinen TAI vuosi on 4 jaollinen, mutta ei 100 jaollinen
+			if (vuosi % 400 == 0 || vuosi % 4 == 0 && vuosi % 100 != 0) {
+				Console.WriteLine(vuosi + " on karkaus vuosi");
+			}
+			else {
+				Console.WriteLine(vuosi + " ei ole karkaus vuosi");
+			}
+		}
+	}
+}
+
