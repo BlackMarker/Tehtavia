bd1d6f62ffbd4d85ce7ee4c0e3b1750e513f472e
Jube <Jube@DESKTOP-MKAEJ4T>
2017-01-12 15:47:18 +0200

    teht4 valm

---
+++ Teht4/Teht4/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Tee ohjelma, jossa kysytään käyttäjältä tämän ikä. 
+ * Jos ikä on alle 18 vuotta, tulosta "alaikäinen", jos se on 18-65 vuotta,
+ *  tulosta "aikuinen", muussa tapauksessa tulosta "seniori".
+ *   alaikäinen < 18
+ *   18 < aikuinen < 65
+ *   else: seniori
+ */
+
+using System;
+
+namespace Teht4{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Kuinka vanha olet: ");
+            int ika;
+            ika = int.Parse(Console.ReadLine());
+			if (ika <= 65){
+				if (ika >= 18){
+					Console.WriteLine("aikuinen");
+				}
+				else{
+					Console.WriteLine("alaikainen");
+				}
+			}
+			else{
+				Console.WriteLine("seniori");
+			}
+        }
+    }
+}
