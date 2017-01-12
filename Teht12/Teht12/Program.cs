504c81f68f02c57e3750ddd5eca48a135ee28e6b
Jube <Jube@DESKTOP-MKAEJ4T>
2017-01-12 20:40:30 +0200

    Teht11 valm

---
+++ Teht12/Teht12/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Tee kahden sisäkkäisen for-silmukan avulla ohjelma, joka tulostaa seuraavanlaisen kuvion:
+*
+**
+***
+****
+*****
+* */
+using System;
+
+
+namespace Teht12 {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Anna luku: ");
+			int maara = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < maara; i++) {
+				for (int j = 0; j < i + 1; j++) {
+					Console.Write("*");
+				}
+				Console.WriteLine("\n");
+			}
+		}
+	}
+}
