81a78be6ba4db3907fafc8a5eb8acdbe68c3b6b7
K8811 <K8811@ICT-421-WS-26>
2017-01-12 12:28:43 +0200

    kesken t1

---
+++ Lotto/Lotto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using class class1
+{
+
+}
+
+namespace Lotto
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Lotto Peli");
+			while (true) {
+				Console.WriteLine("Montako lottorivia haluat: ");
+				string kasky = Console.ReadLine();
+				int kertaa = System.Convert.ToInt32(kasky);
+
+			}
+		}
+	}
+}
---
+++ Teht1/Teht1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) vastaavan luvun sanana (yksi, kaksi tai kolme). 
+ * Jos käyttäjä syöttää jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku". */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1
+{
+	class Program{
+		static void Main(string[] args){
+			//Alustetaan muuttuja
+			while (true) {
+				int syote;
+				Console.WriteLine("Anna numero (1, 2 ,3)");
+				//Tallennetaan muuttujaan käyttäjän syöte
+				syote = Console.Read();
+				string teksti = Convert.ToString(syote);
+				if (teksti == "1" || teksti == "2" || teksti == "3") {
+					Console.WriteLine("Annoit luvun: " + syote);
+				}
+				else {
+					Console.WriteLine("Antamasi luku ei kelpaa");
+				}
+			}
+
+		}
+	}
+}
