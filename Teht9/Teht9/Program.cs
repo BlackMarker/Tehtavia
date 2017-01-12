9122dce2681db96ae791db94bfbe25b83cd5a194
Jube <Jube@DESKTOP-MKAEJ4T>
2017-01-12 20:09:29 +0200

    Teht9 valm

---
+++ Teht9/Teht9/Program.cs
@@ -0,0 +1,26 @@
+﻿//Tee ohjelma, joka kysyy käyttäjältä lukuja, kunnes hän syöttää luvun 0. Ohjelman tulee tulostaa syötettyjen lukujen summa.
+using System;
+using System.Collections.Generic;
+
+namespace Teht8 {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Luettele lukuja, 0 -lopettaa ja kertoo summan");
+			int tulos = 0;
+			//luodaan dynaaminen lista
+			List <int> lista = new List<int>();
+			while (true) {
+				int luku = int.Parse(Console.ReadLine());
+				if (luku == 0) {
+					break;
+				}
+				lista.Add(luku);
+			}
+			//foreach käy listan jokaisen kohdan läpi ja lisää muuttujaan tulos
+			foreach (int kohta in lista) {
+				tulos = tulos + kohta;
+			}
+			Console.Write("lukujen summa on: " + tulos + "\n");
+		}
+	}
+}
