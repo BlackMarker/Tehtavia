10a14f8cdf5bb53ffc891d3a9fb07c8f41e5fc7c
Jube <Jube@DESKTOP-MKAEJ4T>
2017-01-14 19:36:44 +0200

    Teht 15 kesken

---
+++ Teht15/Teht15/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+/* Tee ohjelma, joka tulostaa seuraavanlaisen kuvion. Kysy käyttäjältä puunkorkeus käytä juurena kaksi korkeuden yksikkö.
+
+
+    Anna Luku > 7 [Enter]    
+        *        
+       ***
+      *****
+     *******
+    *********
+        *
+        */
+
+namespace Teht15 {
+
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Kuinka korkea puu: ");
+			int korkeus = int.Parse(Console.ReadLine());
+			korkeus--;
+			int leveys = korkeus;
+			int teko = 0;
+			//Muuttuja tähtien määrän kasvamisella
+			int maara = 1;
+			//Huijausapuri
+
+			/*/For looppi korkeus riveille
+			for (int i = 0; i < korkeus -2; i++) {
+				//For looppi space maaralle
+				for (int j = leveys; j > 0; j--) {
+					Console.Write(" ");
+					//Fof looppi tahdille
+					for (int k = 0; k < korkeus; k++) {
+						Console.Write("*");
+					}
+				}
+				Console.WriteLine("\n");
+			}
+			*/
+			//spacet
+			if (teko == 0) {
+				for (int i = leveys; i > -1; i--) {
+					Console.Write(" ");
+				}
+				teko = 1;
+			}
+			//tähdet
+			if (teko == 1) {
+				for (int i = maara; i > 0; i--) {
+					Console.Write("*");
+				}
+				teko = 2;
+			}
+			if (teko == 2) {
+				Console.WriteLine();
+				teko = 0;
+			}
+				while (true) {
+				//spacet
+				if (teko == 0) {
+					for (int i = leveys; i > 0; i--) {
+						Console.Write(" ");
+					}
+					teko = 1;
+				}
+				//tähdet
+				if (teko == 1) {
+					for (int i = maara; i > 0; i--) {
+						Console.Write("**");
+					}
+					maara++;
+					leveys--;
+					teko = 2;
+				}
+				if (teko == 2) {
+					Console.WriteLine();
+					teko = 0;
+				}
+				if (maara == korkeus) {
+					break;
+				}
+			}
+		}
+	}
+}
